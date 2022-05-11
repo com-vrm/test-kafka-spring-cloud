31d8d07f8272cad987632991c3564ce42479e6b5
taaviv <taavi.vreimann@reach-u.com>
2022-05-11 14:08:24 +0300

    FutureCallback added

--- src/main/java/com/vrm/test/kafka/producer/AbstractProducer.java
+++ src/main/java/com/vrm/test/kafka/producer/AbstractProducer.java
@@ -2,11 +2,15 @@ package com.vrm.test.kafka.producer;
 
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
 
 /**
  * @author taaviv @ 09.05.22
  */
+@Slf4j
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class AbstractProducer<K, V> implements Producer<V> {
 
@@ -17,7 +21,10 @@ public abstract class AbstractProducer<K, V> implements Producer<V> {
     @Override
 
     public void produce(V v) {
-        template.send(topic, keyFactory.nextKey(), v);
+        ListenableFuture<SendResult<K, V>> future = template.send(topic, keyFactory.nextKey(), v);
+        future.addCallback(
+                result -> log.info("Message was consumed: {}", result),
+                ex -> log.error("Message wes not consumed", ex));
     }
 
 }
